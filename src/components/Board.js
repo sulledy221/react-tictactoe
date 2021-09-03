2e6d4774605631a81f9695385c9b0368ff3f221f
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-09-03 13:21:48 -0700

    setting up the components folder

---
+++ src/components/Board.js
@@ -0,0 +1,13 @@
+import Square from "./Square";
+
+
+
+export default function Board({ squares, onClick}) {
+  return (
+    <div className="board">
+      {squares.map((square, i) => (
+        <Square key={i} value={square} onClick={() => onClick(i)}/>
+      ))}
+    </div>
+  )
+}
---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+
+
+
+export default function Square({ value, onClick }) {
+  const style = value ? `squares ${value}` : `squares`;
+  return (
+    <button className={style}onClick={onClick}>{value}</button>
+  )
+}
