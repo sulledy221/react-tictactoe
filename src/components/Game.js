22330352d60184d044adce9add5a5bd5a2da9bf6
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-09-03 14:44:46 -0700

    tic tac toe board rendering on the page and functioning

--- src/components/Game.js
+++ src/components/Game.js
@@ -9,10 +9,51 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const winner = calculateWinner(history[stepNumber]);
   const xO = xIsNext ? "X" : "O";
-  const handleClick = (i) => {
-    const  historyPoint = history.clice(0, stepNumber + 1);
-  }
-  return (
 
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+    // return if won or occupied
+    if (winner || squares[i]) return;
+    // select square
+    squares[i] = xO;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXIsNext(!xIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0)
+  }
+
+const renderMoves = () => {
+  history.map((_step, move) => {
+    const destination = move ? `Go to move #${move}` : "Go to Start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{destination}</button>
+      </li>
+    );
+  });
+}
+
+  return (
+    <>
+    <h1>Tik-tak-toe</h1>
+    <Board squares={history[stepNumber]} onClick={handleClick}/>
+    <div className="info-wrapper">
+      <div>
+      <h3>History</h3>
+      {renderMoves()}
+      </div>
+    <h3>
+      {winner ? "Winner: " + winner : "Next Player: " + xO}
+    </h3>
+    </div>
+
+    </>
   )
+
 }
